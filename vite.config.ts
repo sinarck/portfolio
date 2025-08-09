df3db796d5faf2ec0b12f0698e287391ef17d1f7
Aadi <83467783+sinarck@users.noreply.github.com>
2025-08-08 19:13:06 -0500

    feat: add posthog integration

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,30 @@
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
+import { vitePostHog } from "vite-plugin-posthog";
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  process.env = { ...process.env, ...env };
+
+  if (!process.env.VITE_POSTHOG_KEY || !process.env.VITE_POSTHOG_API_HOST) {
+    throw new Error("VITE_POSTHOG_KEY or VITE_POSTHOG_API_HOST is not set");
+  }
+
+  return {
+    plugins: [
+      react(),
+      tailwindcss(),
+      vitePostHog({
+        apiKey: process.env.VITE_POSTHOG_KEY,
+        hostUrl: process.env.VITE_POSTHOG_API_HOST,
+      }),
+    ],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
+  };
 });
-
