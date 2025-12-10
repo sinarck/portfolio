4ab2681f4fc4b13042d24546ee76e24199967e6f
Aadi <83467783+sinarck@users.noreply.github.com>
2025-12-10 02:56:01 -0600

    chore: fix capitalization in portfolio config

--- src/config/portfolio.ts
+++ src/config/portfolio.ts
@@ -33,21 +33,21 @@ export const portfolioData: PortfolioData = {
 		{
 			name: "Open Attendance",
 			description:
-				"an attendance system for schools (and got a few users along the way). Not a fan of proprietary software.",
+				"An attendance system for schools (and got a few users along the way). Not a fan of proprietary software.",
 			link: "https://github.com/sinarck/open-attendance",
 			date: "2025-01-01",
 		},
 		{
 			name: "Hand Wave",
 			description:
-				"Real-time ASL translation with Meta AI glasses. Presented @ texas school for the deaf.",
+				"Real-time ASL translation with Meta AI glasses. Presented @ The Texas School for the Deaf.",
 			link: "https://github.com/sinarck/hand-wave",
 			date: "2025-02-01",
 		},
 		{
 			name: "Storm Shield",
 			description:
-				"A mobile app demo that connects volunteers with disaster relief efforts. Got to show off @ us capitol",
+				"A mobile app demo that connects volunteers with disaster relief efforts. Got to show off @ US Capitol",
 			link: "https://www.stormshield.app",
 			date: "2024-01-01",
 		},
