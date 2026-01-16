e647fb89d1b6fcdc07a7c993bd39e273b895d9c5
Aadi <83467783+sinarck@users.noreply.github.com>
2026-01-16 08:50:22 -0600

    fix: import issue (again)

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,7 +1,7 @@
 import { TanStackDevtools } from "@tanstack/react-devtools";
 import { createRootRoute, HeadContent, Scripts } from "@tanstack/react-router";
 import { TanStackRouterDevtoolsPanel } from "@tanstack/react-router-devtools";
-import Header from "@/components/Header";
+import Header from "@/components/header";
 import appCss from "../styles.css?url";
 
 export const Route = createRootRoute({
